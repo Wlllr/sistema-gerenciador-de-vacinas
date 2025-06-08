acb02e44783adc4828c938d0b4b3aeb9456d6cde
Weller <weller_pereira@hotmail.com>
2025-06-08 15:35:38 -0300

    Primeiro commit -> Criando a estrutura inicial de packages

---
+++ src/main/java/com/github/wlllr/StartApplication.java
@@ -0,0 +1,13 @@
+package com.github.wlllr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApplication {
+	// é por aqui que a applicacao ira iniciar
+	public static void main(String[] args) {
+		SpringApplication.run(StartApplication.class, args);
+	}
+
+}
